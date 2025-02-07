fd64d95d0331b2b127ad4b5ac8e971e7e913a043
Abhishek Parmar <131667318+2910Abhishek@users.noreply.github.com>
2025-02-07 10:12:50 +0530

    Updated CSV name

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ async def register_user(data: RegistrationData):
 
     try:
         # Check if file exists in Google Drive to verify uniqueness
-        file_name = 'Makerfest.csv'
+        file_name = 'MakerFestVadodara.csv'
         response = drive_service.files().list(
             q=f"name='{file_name}'",
             spaces='drive',
